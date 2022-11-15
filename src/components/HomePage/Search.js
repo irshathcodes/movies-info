6490a14ff88e0f416c6ace83fc7d2baecb626686
Irshath <irshath700@gmail.com>
2022-11-15 20:30:05 +0530

    styles: add padding

--- src/components/HomePage/Search.js
+++ src/components/HomePage/Search.js
@@ -2,30 +2,31 @@ import { useRef, useEffect } from "react";
 import { useMyContextApi } from "../../context";
 
 const Search = () => {
-  const { setQuery, error } = useMyContextApi();
-  const focusRef = useRef("");
+	const { setQuery, error } = useMyContextApi();
+	const focusRef = useRef("");
 
-  useEffect(() => {
-    focusRef.current.focus();
-  }, []);
+	useEffect(() => {
+		focusRef.current.focus();
+	}, []);
 
-  return (
-    <>
-      <div className="search-container">
-        <div className="label-search">search movie</div>
-        <input
-          type="text"
-          className="input-class"
-          placeholder="enter the movie name"
-          ref={focusRef}
-          onChange={(e) => {
-            return setQuery(e.target.value);
-          }}
-        />
-        {error.showError && <h2 className="error-report">{error.msg}</h2>}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="search-container">
+				<div className="label-search">search movie</div>
+				<input
+					type="text"
+					className="input-class"
+					placeholder="enter the movie name"
+					style={{ padding: "1rem" }}
+					ref={focusRef}
+					onChange={(e) => {
+						return setQuery(e.target.value);
+					}}
+				/>
+				{error.showError && <h2 className="error-report">{error.msg}</h2>}
+			</div>
+		</>
+	);
 };
 
 export default Search;
